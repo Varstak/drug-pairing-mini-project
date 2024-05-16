95bc10952d87255ab820b1936dc2c09fb9ae4f0a
Varstak <e0725939@u.nus.edu>
2024-05-16 11:15:04 +0800

    Added Angular frontend

---
+++ drugpairingsystem/src/main/java/sg/synapxe/drugpairingsystem/models/User.java
@@ -0,0 +1,37 @@
+package sg.synapxe.drugpairingsystem.models;
+
+public class User {
+    public long id;
+    public String username;
+    public String password;
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

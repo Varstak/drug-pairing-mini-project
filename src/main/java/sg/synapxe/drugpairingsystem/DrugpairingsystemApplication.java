28b149fa43e8abc6d3658102fb8d8dc4ff974acf
Varstak <e0725939@u.nus.edu>
2024-05-16 09:19:56 +0800

    Init Spring Boot

---
+++ src/main/java/sg/synapxe/drugpairingsystem/DrugpairingsystemApplication.java
@@ -0,0 +1,13 @@
+package sg.synapxe.drugpairingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugpairingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugpairingsystemApplication.class, args);
+	}
+
+}
